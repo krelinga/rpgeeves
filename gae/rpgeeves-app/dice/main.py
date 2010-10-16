cb56f6e393ec9a82b7583faad22a758dcfca0b45
Andy Kreling <krelinga@gmail.com>
2010-10-16 14:37:54 -0700

    Fix tab spacing on dice/main.py.

--- gae/rpgeeves-app/dice/main.py
+++ gae/rpgeeves-app/dice/main.py
@@ -1,18 +1,21 @@
 #! /usr/bin/env python
 
+import os
+
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
+
 class DiceHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Dice!')
+  def get(self):
+    self.response.out.write('Dice!')
 
 
 def main():
-    application = webapp.WSGIApplication([('/dice', DiceHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+  application = webapp.WSGIApplication([('/dice', DiceHandler)], debug=True)
+  util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+  main()
