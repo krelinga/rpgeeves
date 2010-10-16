b1172e27cf3283eeb9761b847f387cc8f902828d
Andy Kreling <krelinga@gmail.com>
2010-10-16 14:36:50 -0700

    Fix tab spacing on top-level main.py.

--- gae/rpgeeves-app/main.py
+++ gae/rpgeeves-app/main.py
@@ -19,15 +19,14 @@ from google.appengine.ext.webapp import util
 
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+  def get(self):
+    self.response.out.write('Hello world!')
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+  application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+  util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+  main()
