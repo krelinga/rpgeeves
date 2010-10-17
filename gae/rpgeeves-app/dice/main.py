53a81b59b3990491707bf3a9717d769a88021f1b
Andy Kreling <krelinga@gmail.com>
2010-10-16 19:49:44 -0700

    Hook dice expression evaluation up with web form /dice.

--- gae/rpgeeves-app/dice/main.py
+++ gae/rpgeeves-app/dice/main.py
@@ -6,12 +6,22 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
+import dice.expression as expression
+
 
 class DiceHandler(webapp.RequestHandler):
   def get(self):
-    d = self.request.get('d')
+    d = self.request.get('d').encode('ascii')
+    tpl_dict = {'d' : d}
+    try:
+      entries = expression.Evaluate(d)
+      tpl_dict['total']  = sum([int(x) for x in entries])
+      tpl_dict['entries'] = [str(x) for x in entries]
+    except expression.ParseError, e:
+      tpl_dict['error'] = str(e)
+
     tpl_path = os.path.join(os.path.dirname(__file__), 'dice.html')
-    self.response.out.write(template.render(tpl_path, {'d' : d}))
+    self.response.out.write(template.render(tpl_path, tpl_dict))
 
 
 def main():
