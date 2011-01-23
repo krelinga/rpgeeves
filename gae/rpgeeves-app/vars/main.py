6f2954f677dcee3c65e364236bc7e6cd858b3867
Andy Kreling <krelinga@gmail.com>
2011-01-22 20:02:42 -0800

    Add a template for viewing lists of variables.

--- gae/rpgeeves-app/vars/main.py
+++ gae/rpgeeves-app/vars/main.py
@@ -7,7 +7,19 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
+import vars.store as store
+
 class VarsHandler(webapp.RequestHandler):
+  def __HandleNew(self):
+    pass
+
+  def __HandleList(self):
+    tpl_path = os.path.join(os.path.dirname(__file__), 'var_list.html')
+    owner = users.get_current_user()
+    tpl_dict = {'vars' : [store.DiceVar(owner=owner, name="foo", value="1d6"),
+                          store.DiceVar(owner=owner, name="bar", value="1d20")]}
+    self.response.out.write(template.render(tpl_path, tpl_dict))
+    pass
   def get(self):
     # make sure the user is logged in
     user = users.get_current_user()
@@ -15,7 +27,13 @@ class VarsHandler(webapp.RequestHandler):
       self.redirect(users.create_login_url(self.request.uri))
       return
 
-    self.response.out.write('hello vars!')
+    action = self.request.get('action').encode('ascii')
+    if action == 'new':
+      self.__HandleNew()
+      return
+    else:
+      self.__HandleList()
+      return
 
 
 def main():
