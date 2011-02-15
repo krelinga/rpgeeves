ea23196332df2d6a195220ac928568e405f319b8
Andy Kreling <krelinga@gmail.com>
2011-02-14 23:26:07 -0800

    Add expander logic and tests for expressions.

--- gae/rpgeeves-app/vars/expander.py
+++ gae/rpgeeves-app/vars/expander.py
@@ -1,4 +1,20 @@
 #! /usr/bin/env python
 
+import re
+
 def RequiresExpansion(expression):
   return '$' in expression
+
+class Expander:
+  def __init__(self, lookup_fn):
+    self.__lookup_fn = lookup_fn
+
+  def Expand(self, expression):
+    while RequiresExpansion(expression):
+      done_vars = set()
+      for var in [x.lstrip('$') for x in re.findall(r'\$\w+', expression)]:
+        if var in done_vars:
+          continue
+        result = self.__lookup_fn(var)
+        expression = expression.replace('$%s' % var, result)
+    return expression
--- gae/rpgeeves-app/vars/expander_test.py
+++ gae/rpgeeves-app/vars/expander_test.py
@@ -3,6 +3,7 @@
 import unittest
 
 from expander import RequiresExpansion
+from expander import Expander
 
 class TestRequiresExpansion(unittest.TestCase):
   def test_WithVars(self):
@@ -10,6 +11,38 @@ class TestRequiresExpansion(unittest.TestCase):
 
   def test_WithoutVars(self):
     self.assertEqual(False, RequiresExpansion('bar'))
+
+class TestExpander(unittest.TestCase):
+  @staticmethod
+  def ExceptionOnExpand(expr):
+    raise Exception('called with expr: %s' % expr)
+
+  def test_NoVars(self):
+    instance = Expander(TestExpander.ExceptionOnExpand)
+    to_expand = 'no vars here'
+    self.assertEqual(to_expand, instance.Expand(to_expand))
+
+  def test_OneLevelVars(self):
+    mapping = {'foo' : '1',
+               'bar' : '2',
+               'baz' : '3'}
+    instance = Expander(lambda x: mapping[x])
+    self.assertEqual('1 2 3', instance.Expand('$foo $bar $baz'))
+
+  def test_DuplicateVars(self):
+    mapping = {'foo' : '1',
+               'bar' : '2'}
+    instance = Expander(lambda x: mapping[x])
+    self.assertEqual('1 2 1', instance.Expand('$foo $bar $foo'))
+
+  def test_NestedVars(self):
+    mapping = {'foo' : '1',
+               'bar' : '2',
+               'baz' : '3',
+               'taters' : '$foo $bar',
+               'pie' : '$bar $baz'}
+    instance = Expander(lambda x: mapping[x])
+    self.assertEqual('1 2 2 3', instance.Expand('$taters $pie'))
 
 
 if __name__ == '__main__':
