915c0016c05ecd4390a7f80d7a441bd73cffb9bd
Andy Kreling <krelinga@gmail.com>
2011-02-15 00:16:04 -0800

    Hook the expander into /dice.

--- gae/rpgeeves-app/dice/main.py
+++ gae/rpgeeves-app/dice/main.py
@@ -2,11 +2,14 @@
 
 import os
 
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
 import dice.expression as expression
+import vars.expander as expander
+import vars.store as store
 
 
 class DiceHandler(webapp.RequestHandler):
@@ -17,14 +20,40 @@ class DiceHandler(webapp.RequestHandler):
     else:
       return str(entry)
 
+  class Exception(Exception):
+    pass
+
+  def __ExpandVars(self, to_expand):
+    if not expander.RequiresExpansion(to_expand):
+      return to_expand
+    assert users.get_current_user()
+    query = store.User.all()
+    query.filter('username =', users.get_current_user())
+    results = query.fetch(1)
+    if len(results) == 0:
+      raise DiceHandler.Exception('You haven\'t created any variables yet!')
+    user = results[0]
+    try:
+      expand_fn = lambda x: expander.ExpandFromStore(x, user)
+      expander_instnace = expander.Expander(expand_fn)
+      return expander_instnace.Expand(to_expand)
+    except expander.UnknownVariableException, e:
+      raise DiceHandler.Exception(str(e))
+
   def get(self):
     d = self.request.get('d').encode('ascii')
     tpl_dict = {'d' : d}
+
+    if expander.RequiresExpansion(d) and not users.get_current_user():
+      self.redirect(users.create_login_url(self.request.uri))
+      return
+
     try:
-      entries = expression.Evaluator().Evaluate(d)
+      inlined = self.__ExpandVars(d).encode('ascii')
+      entries = expression.Evaluator().Evaluate(inlined)
       tpl_dict['total']  = sum([int(x) for x in entries])
       tpl_dict['entries'] = [self.__EntryHtml(x) for x in entries]
-    except expression.ParseError, e:
+    except DiceHandler.Exception, e:
       tpl_dict['error'] = str(e)
 
     tpl_path = os.path.join(os.path.dirname(__file__), 'dice.html')
--- gae/rpgeeves-app/vars/expander.py
+++ gae/rpgeeves-app/vars/expander.py
@@ -2,6 +2,9 @@
 
 import re
 
+from store import DiceVar
+from store import User
+
 def RequiresExpansion(expression):
   return '$' in expression
 
@@ -18,3 +21,18 @@ class Expander:
         result = self.__lookup_fn(var)
         expression = expression.replace('$%s' % var, result)
     return expression
+
+
+class UnknownVariableException(Exception):
+  def __init__(self, variable):
+    Exception.__init__(self, 'Unknown variable %s' % variable)
+
+
+def ExpandFromStore(variable, user):
+  query = DiceVar.all()
+  query.filter('owner =', user)
+  query.filter('name =', variable)
+  results = query.fetch(1)
+  if len(results) == 0:
+    raise UnknownVariableException(variable)
+  return results[0].value
