a3fa07285366ccfba9c2e6325d1ebb3931ad7ce2
Andy Kreling <krelinga@gmail.com>
2011-09-05 16:03:10 -0700

    Remove playground gae stuff.

--- gae/rpgeeves-app/dice/main.py
+++ gae/rpgeeves-app/dice/main.py
@@ -22,8 +22,6 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 
 import dice.expression as expression
-import vars.expander as expander
-import vars.store as store
 
 
 class DiceHandler(webapp.RequestHandler):
@@ -37,23 +35,6 @@ class DiceHandler(webapp.RequestHandler):
   class Exception(Exception):
     pass
 
-  def __ExpandVars(self, to_expand):
-    if not expander.RequiresExpansion(to_expand):
-      return to_expand
-    assert users.get_current_user()
-    query = store.User.all()
-    query.filter('username =', users.get_current_user())
-    results = query.fetch(1)
-    if len(results) == 0:
-      raise DiceHandler.Exception('You haven\'t created any variables yet!')
-    user = results[0]
-    try:
-      expand_fn = lambda x: expander.ExpandFromStore(x, user)
-      expander_instnace = expander.Expander(expand_fn)
-      return expander_instnace.Expand(to_expand)
-    except expander.UnknownVariableException, e:
-      raise DiceHandler.Exception(str(e))
-
   def __EvaluateExpression(self, to_evaluate):
     try:
       return expression.Evaluator().Evaluate(to_evaluate)
@@ -64,13 +45,8 @@ class DiceHandler(webapp.RequestHandler):
     d = self.request.get('d').encode('ascii')
     tpl_dict = {'d' : d}
 
-    if expander.RequiresExpansion(d) and not users.get_current_user():
-      self.redirect(users.create_login_url(self.request.uri))
-      return
-
     try:
-      inlined = self.__ExpandVars(d).encode('ascii')
-      entries = self.__EvaluateExpression(inlined)
+      entries = self.__EvaluateExpression(d)
       tpl_dict['total']  = sum([int(x) for x in entries])
       tpl_dict['entries'] = [self.__EntryHtml(x) for x in entries]
     except DiceHandler.Exception, e:
