36d3a44ef8b60f1490b159f6c110a3e6a4aa2665
Andy Kreling <krelinga@gmail.com>
2011-09-11 16:23:51 -0700

    Add server-side persistence of dice expressions.

--- gae/rpgeeves-app/js_dice/main.py
+++ gae/rpgeeves-app/js_dice/main.py
@@ -14,26 +14,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import os
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+from google.appengine.ext import db
+
+
+class ExpressionSet(db.Model):
+  """A single string containing the serialized json for the user's dice."""
+  user = db.UserProperty()
+  expression_set = db.StringProperty(multiline=True)
 
 
 class JsDiceEditHandler(webapp.RequestHandler):
+  def getExpressionSetForUser(self, user):
+    expression_set_result = db.GqlQuery('SELECT *'
+                                        'FROM ExpressionSet '
+                                        'WHERE user = :1 '
+                                        'LIMIT 1', user)
+    for result in expression_set_result:
+      return result
+    return None
+
   def get(self):
     """Create a page to allows users to edit stored sets of dice."""
+    # Make sure the user is logged in.
+    user = users.get_current_user()
+    if not user:
+      self.redirect(users.create_login_url(self.request.uri))
+      return
+
+    # lookup existing expression set for this user
+    result = self.getExpressionSetForUser(user)
+    if result:
+      starting_set_json = result.expression_set
+    else:
+      starting_set_json = '{}'
+      
     tpl_path = os.path.join(os.path.dirname(__file__), 'js_dice_edit.html')
     tpl_dict = {
-      'starting_set_json': '{"foo": "d20","bar": "2d10 + 3","baz": "3d4"}',
+      'starting_set_json': starting_set_json,
     }
     self.response.out.write(template.render(tpl_path, tpl_dict))
 
   def post(self):
     """Accept a user-supplied edit of a stored set of dice."""
-    self.response.out.write('not implemented yet')
+    logging.info(self.request.body)
+    user = users.get_current_user()
+    if not user:
+      self.response.set_status(401)
+
+    result = self.getExpressionSetForUser(user)
+    if result:
+      result.expression_set = self.request.body
+    else:
+      result = ExpressionSet()
+      result.user = user
+      result.expression_set = self.request.body
+    result.put()
+    self.response.set_status(200)
 
 
 class JsDiceHandler(webapp.RequestHandler):
